bbe62da3a721c935ff7cb79327d4cc158bc60e71
Sheldon Hull <sheldonhull@users.noreply.github.com>
2021-08-26 13:07:32 -0500

    feat: 🎉 submitted gigasecond solution

--- exercism.io/go/gigasecond/gigasecond.go
+++ exercism.io/go/gigasecond/gigasecond.go
@@ -1,18 +1,16 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package gigasecond
 
 // import path for the time package from the standard library
-import "time"
+import (
+	"time"
+)
 
-// AddGigasecond should have a comment documenting it.
+// gigasecond represents a very very very small portion of a second.
+const gigasecond = 1000000000
+
+// AddGigasecond adds a very very very small portion of a second called a gigasecond to a provided time input.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	gcDuration := gigasecond * time.Second
+	n := t.Add(gcDuration)
+	return n
 }
